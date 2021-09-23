bb52f63f3c3de3b7e86ba9bbd1272c345bb1f46f
imAddyxD <89731805+imAddyxD@users.noreply.github.com>
2021-09-23 11:04:02 +0530

    Removed Telegraph Upload

--- bot.py
+++ bot.py
@@ -2,9 +2,6 @@
 #sys
 import os, logging, asyncio
 
-#telegraph 
-from telegraph import upload_file
-
 #telethon bhaiya
 from telethon import Button
 from telethon import TelegramClient, events
@@ -121,46 +118,6 @@ async def mentionall(event):
         usrtxt = ""
 
 
-#telegraph 
-@client.on(events.NewMessage(pattern="^/t$"))
-async def telegraph(client, message):
-    replied = message.reply_to_message
-    if not replied:
-        await message.reply("Reply to a supported media file")
-        return
-    if not (
-        (replied.photo and replied.photo.file_size <= 5242880)
-        or (replied.animation and replied.animation.file_size <= 5242880)
-        or (
-            replied.video
-            and replied.video.file_name.endswith(".mp4")
-            and replied.video.file_size <= 5242880
-        )
-        or (
-            replied.document
-            and replied.document.file_name.endswith(
-                (".jpg", ".jpeg", ".png", ".gif", ".mp4"),
-            )
-            and replied.document.file_size <= 5242880
-        )
-    ):
-        await message.reply("Not supported!")
-        return
-    download_location = await client.download_media(
-        message=message.reply_to_message,
-        file_name="root/downloads/",
-    )
-    try:
-        response = upload_file(download_location)
-    except Exception as document:
-        await message.reply(message, text=document)
-    else:
-        await message.reply(
-            f"**Hey You...!\nLoook At This\n\n👉 https://telegra.ph{response[0]}**",
-            disable_web_page_preview=True,
-        )
-    finally:
-        os.remove(download_location)
 
 print("~~~~Started~~~~~")
 print("¯\_(ツ)_/¯ Need Help Join @RhythmOff")
